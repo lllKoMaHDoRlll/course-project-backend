bb250926f15c40e38550cdbf5c5513fafc45d1b7
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-10-23 19:52:20 +0300

    implement user models

---
+++ models/users.py
@@ -0,0 +1,16 @@
+from typing import TypedDict
+from pydantic import BaseModel
+
+class TotalStats(TypedDict):
+    user_id: int
+    last_entrance_date: str
+    entrance_streak: int
+
+class User(TypedDict):
+    id: int
+    wallet: str | None
+
+class UserData(BaseModel):
+    user_id: int
+    wallet: str | None
+
