e21b8ae46522fb305211242c16a12edc2350e95e
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-10-23 19:52:57 +0300

    implement achievements enum

--- models/achievements.py
+++ models/achievements.py
@@ -1,6 +1,29 @@
 from typing import TypedDict
+from enum import Enum
 
 
 class AchievementType(TypedDict):
     id: int
     name: str
+
+class ACHIEVEMENTS(Enum):
+    START_OF_JOURNEY = 1
+    INTRESTED = 2
+    GURU = 3
+    SPRINTER = 4
+    MARATHON_RUNNER = 5
+    FOREIGNER = 6
+    KNOWING = 7
+    NATIVE_SPEAKER = 8
+    SENTENCES_START = 9
+    SENTENCES_ADVANCED = 10
+    SENTENCES_EXPERT = 11
+    WORDS_START = 12
+    WORDS_ADVANCED = 13
+    WORDS_EXPERT = 14
+    LISTENING_START = 15
+    LISTENING_ADVANCED = 16
+    LISTENING_EXPERT = 17
+    GRAMAR_START = 18
+    GRAMAR_ADVANCED = 19
+    GRAMAR_EXPERT = 20
