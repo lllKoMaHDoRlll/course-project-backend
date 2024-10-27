7d32d9ff294e92166c3ea2a523b11cbc93436483
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-10-27 13:52:35 +0300

    implement correct parametr usage in sql queries

--- utils/database.py
+++ utils/database.py
@@ -38,7 +38,7 @@ class Database:
 
     def get_listening_exercise_by_id(self, id: int) -> ExerciseListeningDBData | None:
         if not isinstance(id, int): return None
-        self._cursor.execute(f"SELECT * from listening_exercises WHERE id = {id};")
+        self._cursor.execute("SELECT * from listening_exercises WHERE id = %s;", (id))
         result = self._cursor.fetchone()
         print(result)
         if result: 
@@ -47,14 +47,14 @@ class Database:
     
     def write_listening_exercise(self, exercise: ExerciseListeningDBData) -> int:
         words = " ".join(exercise["words"])
-        self._cursor.execute(f'INSERT INTO listening_exercises (words) VALUES ("{words}");')
+        self._cursor.execute("INSERT INTO listening_exercises (words) VALUES (%s);", (words))
         self._connection.commit()
         last_insert_id = self._cursor.lastrowid
         return last_insert_id
 
     def get_words_exercise_by_id(self, id: int) -> ExerciseWordsDBData | None:
         if not isinstance(id, int): return None
-        self._cursor.execute(f"SELECT * from words_exercises WHERE id = {id};")
+        self._cursor.execute("SELECT * from words_exercises WHERE id = %s;", (id))
         result = self._cursor.fetchone()
         if result:
             return {"id": result[0], "words": result[1].split(), "translations": result[2].split()}
@@ -63,14 +63,14 @@ class Database:
     def write_words_exercise(self, exercise: ExerciseWordsDBData) -> int:
         words = " ".join(exercise["words"])
         translations = " ".join(exercise["translations"])
-        self._cursor.execute(f'INSERT INTO words_exercises (words, translations) VALUES ("{words}", "{translations}");')
+        self._cursor.execute("INSERT INTO words_exercises (words, translations) VALUES (%s, %s);", (words, translations))
         self._connection.commit()
         last_insert_id = self._cursor.lastrowid
         return last_insert_id
 
     def get_sentence_exercise_by_id(self, id: int) -> ExerciseSentenceDBData | None:
         if not isinstance(id, int): return None
-        self._cursor.execute(f"SELECT * from sentence_exercises WHERE id = {id};")
+        self._cursor.execute("SELECT * from sentence_exercises WHERE id = %s;", (id))
         result = self._cursor.fetchone()
         if result:
             return {"id": result[0], "sentence": result[1], "translation": result[2]}
@@ -79,7 +79,7 @@ class Database:
     def write_sentence_exercise(self, exercise: ExerciseSentenceDBData) -> int:
         sentence = exercise["sentence"]
         translation = exercise["translation"]
-        self._cursor.execute(f'INSERT INTO sentence_exercises (sentence, translation) VALUES ("{sentence}", "{translation}");')
+        self._cursor.execute("INSERT INTO sentence_exercises (sentence, translation) VALUES (%s, %s);", (sentence, translation))
         self._connection.commit()
         last_insert_id = self._cursor.lastrowid
         return last_insert_id
@@ -88,7 +88,7 @@ class Database:
         self._cursor.execute("SELECT id from gramar_exercises;")
         ids = list(map(lambda t: t[0], self._cursor.fetchall()))
         random_id = random.choice(ids)
-        self._cursor.execute(f"SELECT * from gramar_exercises WHERE id = {random_id};")
+        self._cursor.execute("SELECT * from gramar_exercises WHERE id = %s;", (random_id))
         result = self._cursor.fetchone()
         if result:
            return {"id": result[0], "description": result[1], "tasks": list(map(lambda task: task.split("%") , result[2].split("@"))), "answers": result[3].split("@")}
@@ -96,7 +96,7 @@ class Database:
     
     def get_gramar_exercise_by_id(self, id: int) -> ExerciseGramarDBData | None:
         if not isinstance(id, int): return None
-        self._cursor.execute(f"SELECT * from gramar_exercises WHERE id = {id};")
+        self._cursor.execute("SELECT * from gramar_exercises WHERE id = %s;", (id))
         result = self._cursor.fetchone()
         if result:
             return {"id": result[0], "description": result[1], "tasks": list(map(lambda task: task.split("%") , result[2].split("@"))), "answers": result[3].split("@")}
@@ -104,7 +104,7 @@ class Database:
     
     def get_completed_exercises_ids_by_user_id(self, exercise_type_id: int, user_id: int) -> list[int]:
         if not isinstance(user_id, int): return []
-        self._cursor.execute(f"SELECT exercise_id from completed_exercises WHERE tg_user_id = {user_id} AND exercise_type_id = {exercise_type_id};")
+        self._cursor.execute("SELECT exercise_id from completed_exercises WHERE tg_user_id = %s AND exercise_type_id = %s;", (user_id, exercise_type_id))
         result = []
         for (exercise_id) in self._cursor:
             result.append(exercise_id)
@@ -114,18 +114,18 @@ class Database:
         description = exercise["description"]
         tasks = "@".join(map(lambda task: "%".join(task), exercise["tasks"]))
         answers = "@".join(exercise["answers"])
-        self._cursor.execute(f'INSERT INTO gramar_exercises (description, tasks, answers) VALUES ("{description}", "{tasks}", "{answers}");')
+        self._cursor.execute("INSERT INTO gramar_exercises (description, tasks, answers) VALUES (%s, %s, %s);", (description, tasks, answers))
         self._connection.commit()
         last_insert_id = self._cursor.lastrowid
         return last_insert_id
     
     def complete_exercise(self, user_id: int, exercise_id: int, exercise_type_id: EXERCISES_TYPES):
-        self._cursor.execute(f'INSERT INTO completed_exercises (tg_user_id, exercise_id, exercise_type_id) VALUES ({user_id}, {exercise_id}, {exercise_type_id.value});')
+        self._cursor.execute("INSERT INTO completed_exercises (tg_user_id, exercise_id, exercise_type_id) VALUES (%s, %s, %s);", (user_id, exercise_id, exercise_type_id.value))
         self._connection.commit()
     
     def get_user_total_stats(self, user_id: int) -> TotalStats | None:
         if not isinstance(user_id, int): return None
-        self._cursor.execute(f"SELECT * from total_stats WHERE tg_user_id = {user_id};")
+        self._cursor.execute("SELECT * from total_stats WHERE tg_user_id = %s;", (user_id))
         result = self._cursor.fetchone()
         print(result)
         if result: 
@@ -138,20 +138,20 @@ class Database:
             
     
     def update_user_total_stats(self, total_stats: TotalStats):
-        self._cursor.execute(f'UPDATE total_stats SET last_entrance_date={total_stats["last_entrance_date"]}, entrance_streak={total_stats["entrance_streak"]} WHERE tg_user_id={total_stats["user_id"]}')
+        self._cursor.execute("UPDATE total_stats SET last_entrance_date=%s, entrance_streak=%s WHERE tg_user_id=%s", (total_stats["last_entrance_date"], total_stats["entrance_streak"], total_stats["user_id"]))
         self._connection.commit()
     
     def write_or_update_user(self, user: User):
-        self._cursor.execute(f'SELECT (tg_user_id) from users WHERE tg_user_id={user["id"]};')
+        self._cursor.execute("SELECT (tg_user_id) from users WHERE tg_user_id=%s;", (user["id"]))
         self._cursor.fetchall()
         if self._cursor.rowcount == 1:
-            self._cursor.execute(f'UPDATE users SET wallet="{user["wallet"]}" WHERE tg_user_id={user["id"]};')
+            self._cursor.execute("UPDATE users SET wallet=%s WHERE tg_user_id=%s;", (user["wallet"], user["id"]))
         else:
-            self._cursor.execute(f"INSERT INTO users (tg_user_id) VALUES ({user['id']});")
+            self._cursor.execute("INSERT INTO users (tg_user_id) VALUES (%s);", (user["id"]))
 
             today = datetime.today().strftime("%Y-%m-%d")
 
-            self._cursor.execute(f"INSERT INTO total_stats (tg_user_id, last_entrance_date, entrance_streak, completed_tasks) VALUES ({user['id']}, {today}, 1, 0);")
+            self._cursor.execute("INSERT INTO total_stats (tg_user_id, last_entrance_date, entrance_streak, completed_tasks) VALUES (%s, %s, %s);", (user["id"], today, 1))
         self._connection.commit()
 
 
