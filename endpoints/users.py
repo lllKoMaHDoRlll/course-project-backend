34ee3c35d799e9e9182dfeee1aa013bc479f9ca5
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-11-06 20:07:13 +0300

    implement create_or_update_user endpoint

---
+++ endpoints/users.py
@@ -0,0 +1,7 @@
+from main import app, database
+
+from models.users import UserData, User
+
+@app.post("/users")
+async def create_or_update_user(user: UserData):
+    database.write_or_update_user(User(id=user.user_id, wallet=user.wallet))
